bbee829c70281963690b023a4c262e13ac69f179
Misha5757 <ra107696@gmail.com>
2021-03-26 14:13:41 +0300

    Session1

--- WpfApp6/Page1.xaml.cs
+++ WpfApp6/Page1.xaml.cs
@@ -27,13 +27,13 @@ namespace WpfApp6
 
         private void Serach_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Ss.ItemsSource = Class1.db2.Compucter.Where(Item => Item.CPU == CmBox.Text && Item.ID.ToString().Contains(Serach.Text)).ToList();
+            Ss.ItemsSource = Class1.db2.Compucter.Where(Item => Item.CPU == CmBox.Text && Item.ID.ToString().Contains(Serach.Text)  ).ToList();
            
         }
 
         private void CmBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Ss.ItemsSource = Class1.db2.Compucter.Where(item => item.CPU.Contains(CmBox.Text) && item.CPU.Contains(CmBox.Text)).ToList();
+            Ss.ItemsSource = Class1.db2.Compucter.Where(item => item.CPU.Contains(CmBox.Text)).ToList();
             //    SS2();
             //    MessageBox.Show(CmBox.Text);
 
@@ -56,6 +56,20 @@ namespace WpfApp6
 
         }
 
-     
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ssss.Visibility == Visibility.Visible)
+            {
+                sss33.Text = ssss.Password;
+                ssss.Visibility = Visibility.Hidden;
+                sss33.Visibility = Visibility.Visible;
+            }
+            else if (ssss.Visibility == Visibility.Hidden)
+            {
+                ssss.Visibility = Visibility.Visible;
+                sss33.Visibility = Visibility.Hidden;
+
+            }
+        }
     }
 }
